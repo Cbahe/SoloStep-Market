2094533d2c3d713ddb0a5283d77cd5e876e4bc86
Chemeeka Bahe <bchemeeka@gmail.com>
2024-07-09 16:26:09 -0700

    initial commit

---
+++ book-club/backend/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
