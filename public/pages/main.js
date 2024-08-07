1cd2e784dcd29b42e544a95a8442d0ddde1625aa
Chemeeka Bahe <bchemeeka@gmail.com>
2024-08-07 11:18:50 -0700

    filing in order

---
+++ public/pages/main.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from 'react';
+import { ShoeList } from './ShoeList.js';
+import SignIn from './signin';
+import SignUp from './signup';
+
+
+export const App = () => {
+
+	const [shoe, setShoe] = useState([]);
+	const [data, setData] = useState([]);
+	const [sellShoe, setSellShoe] = useState(null);
+
+
+	async function fetchShoe(){
+		try {
+			const response = await fetch(`${apiURL}/shoe`);
+			const shoeData = await response.json();
+			
+			setShoe(shoeData);
+		} catch (err) {
+			console.log("Oh no an error! ", err)
+		}
+	}
+
+	async function clickHandler(slug) {
+		try {
+		const res = await fetch(`${apiURL}/shoe/${slug}`)
+		const data = await res.json();
+		setSellShoe(data)
+	  } catch (err) {
+		console.log('Oh no an error! ', err)
+	  }
+	}
+async function onDelete(slug){
+	try{
+		const res = await fetch (`${apiURL}/shoe/${slug}`,{
+			method:"DELETE"
+		})
+	}
+		catch(error){
+			console.log('can not delete',error)
+		}
+	}
+
+	useEffect(() => {
+		fetchShoe();
+	}, [data]);
+
+	function handleItemClick(slug) {
+		clickHandler(slug)
+	  }
+	function handleBack() {
+		setSellShoe(null)
+	  }
+	async function deleteItem(shoe){
+			await onDelete(shoe.id);
+	}
+	return (
+		<main>	
+      <h1>SoloShoe Market</h1>
+			<h2>All things Shoe</h2>
+            <button onClick={SignUp}>Sign Up</button>
+            <button onClick={SignIn}>Sign In</button>
+			{sellShoe ? (
+				<div className="shoe-detail">
+    <h3>Name: {sellShoe.name}</h3>
+    <h4>Price: {sellShoe.price}</h4>
+    <h4>Size:{sellShoe.size}</h4>
+    <h4>Category: {sellShoe.category}</h4>
+	<img src={sellShoe.image} alt={sellShoe.name} />
+	<button onClick={handleBack}>Back to Shoe Shop</button>
+	<button onClick= {() => deleteItem(selItem)} >Delete Item</button>
+						  </div>
+			) : (
+				<>
+				<ShoeList items={shoe} onTitleClick={handleItemClick} />
+			
+				</>
+			)}
+		</main>
+	)
+}
