25280fac07639333e41dd03f7b91f12ae22a9fae
Cbahe <104049572+Cbahe@users.noreply.github.com>
2024-08-09 03:18:46 -0700

    doc: changed app.js

--- public/src/App.js
+++ public/src/App.js
@@ -1,27 +1,11 @@
-import React from 'react';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from 'react-router-dom';
-import SignUp from './pages/signup';
-import SignIn from './pages/signin';
-
-
 const router = createBrowserRouter([
   {
     path: '/',
     children: [
       { path: '/', element: <App /> },
-      { path: '/signin', element: <SignIn />, loader },
-      { path: '/signup', element: <SignUp />, loader },
+      { path: '/signin', element: <SignIn /> },
+      { path: '/signup', element: <SignUp /> },
     ],
   },
-])
+]);
 
-function App() {
-  return (
-    <RouterProvider router={router}/>
-  );
-}
-
-export { App };
