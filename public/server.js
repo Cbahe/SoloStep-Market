cb963cfba7836a853f9ae3c281679644fbcdd1a2
Chemeeka Bahe <bchemeeka@gmail.com>
2024-08-09 01:07:45 -0700

    updated public/server

--- public/server.js
+++ public/server.js
@@ -2,14 +2,6 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Handle all other routes and serve the React app
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
 const PORT = process.env.PORT || 1234;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
