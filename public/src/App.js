7ddb6d22150f9e0b1adb07278828764cbc680c5d
Chemeeka Bahe <bchemeeka@gmail.com>
2024-08-09 03:32:13 -0700

    doc:functionAPP

--- public/src/App.js
+++ public/src/App.js
@@ -1,11 +1,23 @@
+import SignIn from "./pages/signin";
+import SignUp from "./pages/signup";
+import App from "./pages/main";
+
 const router = createBrowserRouter([
   {
     path: '/',
     children: [
-      { path: '/', element: <App /> },
+      { path: '/main', element: <App /> },
       { path: '/signin', element: <SignIn /> },
       { path: '/signup', element: <SignUp /> },
     ],
   },
 ]);
 
+function App() {
+    return (
+      <RouterProvider router={router}/>
+    );
+  }
+  
+
+export { App };
