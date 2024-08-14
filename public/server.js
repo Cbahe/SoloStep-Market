8b981c92b08855cdcc2751a77da91d2b5fd8cd8a
Chemeeka Bahe <bchemeeka@gmail.com>
2024-08-14 15:22:14 -0700

    doc: update node version

--- public/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-
-const PORT = process.env.PORT || 1234;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
